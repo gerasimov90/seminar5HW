de5e298dcb6a4d7cec752d3d13c3c14cf3bfd79c
GerasimovVictor <gerasimov.viktor90@gmail.com>
2022-12-18 13:00:41 +0700

    homework

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу,
+которая покажет количество чётных чисел в массиве.
+
+
+int[] GetArray(int size, int minV, int maxV)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minV, maxV + 1);
+    }
+
+    return array;
+}
+
+int[] resultArray = GetArray(6, 100, 999);
+Console.WriteLine(String.Join("; ", resultArray));
+
+int count = 0;
+
+for (int i = 0; i < resultArray.Length; i++)
+{
+   if (resultArray[i] % 2 == 0) count++;
+}
+Console.WriteLine("Количество четных чисел в массиве: " + count);
+*/
+
+/*
+Задача 36: Задайте одномерный массив, заполненный случайными
+числами. Найдите сумму элементов, стоящих на нечётных позициях.
+*/
+
